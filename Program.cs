6e257a5b4e4b4bedcb3b095d0f4ed15130436744
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-05-30 23:29:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using JobAdScraper.Models;
+
+
+namespace JobAdScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var contentString = await httpResponse.Content.ReadAsStringAsync();
+                var posts = JsonConvert.DeserializeObject<List<Post>>(contentString);
+            }
+        }
+    }
+}
